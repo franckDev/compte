e7229ba1bc0a502ece594610ced2e179710d607e
franckDev <developpeur16700@gmail.com>
2016-07-04 12:18:19 +0200

    maj bdd

--- bdd/comptes.sql
+++ bdd/comptes.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Dim 03 Avril 2016 à 15:08
+-- Généré le :  Lun 04 Juillet 2016 à 12:17
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -53,14 +53,6 @@ CREATE TABLE IF NOT EXISTS `employeur` (
   PRIMARY KEY (`IdEmployeur`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=81 ;
 
---
--- Contenu de la table `employeur`
---
-
-INSERT INTO `employeur` (`IdEmployeur`, `RaisonSociale`, `Adresse`, `CodePostal`, `Ville`, `Created`) VALUES
-(68, 'Quasar Conseil', '115 avenue Paul PainLevé', 86100, 'Chatellerault', '2016-04-02'),
-(69, 'Snri', 'Chemin du treuil CS40107', 16700, 'Ruffec', '2016-04-03');
-
 -- --------------------------------------------------------
 
 --
@@ -96,23 +88,6 @@ CREATE TABLE IF NOT EXISTS `salaires` (
   `Created` date NOT NULL,
   PRIMARY KEY (`IdSalaire`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table des salaires' AUTO_INCREMENT=21 ;
-
---
--- Contenu de la table `salaires`
---
-
-INSERT INTO `salaires` (`IdSalaire`, `IdEmployeur`, `dateDebut`, `dateFin`, `datePaie`, `montantBrut`, `montantNet`, `montantNetImp`, `nbreHeureMois`, `nbreHeureTotal`, `divers`, `Created`) VALUES
-(1, 68, '2015-09-14', '2015-09-30', '2015-09-30', '874.51', '687.59', '702.21', '91.00', '91.00', '', '2016-04-02'),
-(2, 68, '2015-10-01', '2015-10-31', '2015-10-30', '1457.55', '1102.03', '1170.40', '151.67', '242.67', '', '2016-04-02'),
-(3, 68, '2015-11-01', '2015-11-30', '2015-11-30', '1457.52', '1085.00', '1170.37', '151.67', '394.34', '', '2016-04-02'),
-(4, 68, '2015-12-01', '2015-12-31', '2015-12-31', '1457.52', '1095.20', '1170.37', '130.67', '525.01', '', '2016-04-02'),
-(5, 68, '2016-01-01', '2016-01-31', '2016-01-31', '1466.65', '1107.59', '1257.03', '151.67', '151.67', '', '2016-04-03'),
-(6, 68, '2016-02-01', '2016-02-29', '2016-02-29', '1466.65', '1080.39', '1257.03', '151.67', '303.34', '', '2016-04-03'),
-(7, 68, '2016-03-01', '2016-03-31', '2016-03-31', '1466.65', '1076.99', '1264.66', '144.67', '448.01', '', '2016-04-03'),
-(8, 69, '2014-08-01', '2014-08-31', '2014-08-31', '3325.57', '2313.42', '2675.43', '132.62', '1130.69', '', '2016-04-03'),
-(9, 69, '2014-07-01', '2014-07-31', '2014-07-31', '1742.68', '1282.33', '1400.84', '152.46', '978.23', '', '2016-04-03'),
-(10, 69, '2014-06-01', '2014-06-30', '2014-06-30', '2545.68', '1287.59', '2047.41', '138.60', '839.63', '', '2016-04-03'),
-(12, 69, '2014-05-01', '2014-05-31', '2014-05-31', '1742.68', '1282.33', '1400.84', '131.67', '707.96', '', '2016-04-03');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
